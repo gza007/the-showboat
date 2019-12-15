d59b961ea40fb3597e995cb0ee0ce9f24b85eeeb
Geevan Patel <geevan.peter.patel@gmail.com>
2019-12-15 18:16:13 +0000

    updated background and league thumbnails

--- src/categories.js
+++ src/categories.js
@@ -5,13 +5,13 @@ import './styles/App.css';
 import MenuBar from './menuBar';
 
 const imagePL = "https://www.footballticketnet.com/files/images/tournaments/Buy-Premier-League-Football-Tickets-FootballTicketNet.png";
-const imageCL = "https://statics.sportskeeda.com/wp-content/uploads/2014/06/uefa-champions-league-2013-1403699681.jpg";
-const imageEFL = "https://www.carlisleunited.co.uk/siteassets/image/efl/efllogo.jpg/Large";
-const imageFA = "http://d2dzjyo4yc2sta.cloudfront.net/?url=images.pitchero.com%2Fui%2F558160%2F1440016156_0.jpg&bg=fff&w=1200&h=630&t=frame";
-const imageEuros = "https://cdn.mos.cms.futurecdn.net/21552bc46f6ffa835499ed671dd8ad2d.jpg";
-const imageWC = "https://adobe99u.files.wordpress.com/2018/06/wc-logo-composite.jpg?quality=100&resize=1240%2C920&strip";
-const imageWSL = "http://www.ciderspace.co.uk/photos/misc/fawsl-logo.jpg";
-const imageAms = "https://koditips.com/wp-content/uploads/european-football-soccer-kodi.png";
+const imageCL = "https://i.pinimg.com/originals/4e/e8/e9/4ee8e9139110201b6e17ac878d1250fd.jpg";
+const imageEFL = "https://upload.wikimedia.org/wikipedia/en/3/37/EFL_Championship.png";
+const imageSA = "http://www.egypttoday.com/images/larg/24859.jpg";
+const imageLL = "https://iscreativestudio.com/wp-content/uploads/2016/08/logotipos4.jpg";
+const imageBL = "https://1000logos.net/wp-content/uploads/2018/10/Bundesliga-logo.jpg";
+const imageWSL = "https://www.underconsideration.com/brandnew/archives/fa_womens_league_logo.jpg";
+const imageFL = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRrHb3E8S7v8qIG4IfJ6C4FRFAeR5WDoiFvxEGeNALabtkCnlda&s";
 
 const Categories = () => (
     <div className="App bg">
@@ -21,14 +21,11 @@ const Categories = () => (
         <Link to="/categories/premier-league"><img className="item" src={imagePL} alt="Premier League" /></Link>
         <Link to="/categories/champions-league"><img className="item" src={imageCL} alt="Champions League" /></Link>
         <Link to="/categories/english-football-league"><img className="item" src={imageEFL} alt="EFL" /></Link>
-        <Link to="/categories/fa-cup"><img className="item" src={imageFA} alt="FA Cup" /></Link>
-        <Link to="/categories/euro-championships"><img className="item" src={imageEuros} alt="Euros" /></Link>
-        <Link to="/categories/world-cup"><img className="item" src={imageWC} alt="World Cup" /></Link>
+        <Link to="/categories/serie-a"><img className="item" src={imageSA} alt="Serie A" /></Link>
+        <Link to="/categories/la-liga"><img className="item" src={imageLL} alt="La Liga" /></Link>
+        <Link to="/categories/bundesliga"><img className="item" src={imageBL} alt="Bundesliga" /></Link>
         <Link to="/categories/wsl"><img className="item" src={imageWSL} alt="Women's Super League" /></Link>
-        <div className="amateur-container">
-        <Link to="/categories/amateur"><img className="item" src={imageAms} alt="Amateur Football" /></Link>
-        <div className="amateur-text">AMATEUR</div>
-        </div>
+        <Link to="/categories/french-ligue-1"><img className="item" src={imageFL} alt="Amateur Football" /></Link>
         </div>
       </header>
     </div>
