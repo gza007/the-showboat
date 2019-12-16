2399b3006ffb1a3c32da70f6838ffe8b7d69fb47
Geevan Patel <geevan.peter.patel@gmail.com>
2019-12-16 01:48:28 +0000

    sep goals rendering, unable to cycle months as of yet

--- src/goalsContainer.js
+++ src/goalsContainer.js
@@ -11,8 +11,10 @@ import MenuBar from './menuBar';
 
 library.add(faArrowLeft, faArrowRight);
 
-const apiKey = `AIzaSyBdi5Q3kByqWn-UYNznAvsZMu_Bs5YGWPs`;
-const channelID = `UCXDkshUQ8OFJjz2BKGjt-KQ`;
+const apiKey = 'AIzaSyALQKUbE2A6kTaLwuoM_izlhXWPCyd9Ni0'; 
+// old API `AIzaSyBdi5Q3kByqWn-UYNznAvsZMu_Bs5YGWPs`;
+const channelID = 'UCn4VbFTUFSxoQjTBHsuqkLQ';
+// old Channel ID `UCXDkshUQ8OFJjz2BKGjt-KQ`;
 const playlistIDsURL = `https://www.googleapis.com/youtube/v3/playlists?part=snippet&channelId=`;
 const playlistItemsURL = `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=`
 const videoDetailsURL = `https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=`
@@ -22,71 +24,92 @@ class Goals extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      displayMonth: "March",
+      displayMonth: "September",
       playlistInfo: {},
-      marchVideoList: {},
-      aprilVideoList: {},
-      marchVideoDetails: {},
-      marchVideosHere: false,
-      aprilVideoDetails: {},
-      aprilVideosHere: false,
+      novemberVideoList: {},
+      novemberVideoDetails: {},
+      novemberVideosHere: false,
+      octoberVideoList: {},
+      octoberVideoDetails: {},
+      octoberVideosHere: false,
+      septemberVideoList: {},
+      septemberVideoDetails: {},
+      septemberVideosHere: false,
       // eslint-disable-next-line react/no-unused-state
       isError: false,
     };
   }
 
   changeMonthLeft = () => {
-    if (this.state.displayMonth = "April")  {
+    if (this.state.displayMonth = "September")  {
       this.setState({
-        displayMonth: "March"
-      })
-    } else {
+        displayMonth: "November"
+      });
+    } else if (this.state.displayMonth = "November") {
       this.setState({
-        displayMonth: "April"
-      })
-    };
+        displayMonth: "October"
+      });
+    } else
+      this.setState({
+        displayMonth: "September"
+      });
   };
 
   changeMonthRight = () => {
-    if (this.state.displayMonth = "March")  {
+    if (this.state.displayMonth = "November")  {
       this.setState({
-        displayMonth: "April"
+        displayMonth: "September"
       })
-    } else {
+    } else if (this.state.displayMonth = "September") {
       this.setState({
-        displayMonth: "March"
-      })
-    };
+        displayMonth: "October"
+      });
+    } else if (this.state.displayMonth = "October") {
+      this.setState({
+        displayMonth: "November"
+      });
+    }
   };
 
   getData = () => {
-    fetch(`${playlistIDsURL}${channelID}&key=${apiKey}`)
+    fetch(`${playlistIDsURL}${channelID}&key=${apiKey}&maxResults=50`)
       .then((res) => res.json())
       .then(data => {
         this.setState({ playlistInfo: data })
         fetch(`${playlistItemsURL}${data.items[0].id}&key=${apiKey}`)
       .then((res2) => res2.json())
       .then(data2 => {
-        this.setState({ marchVideoList: data2 });
-        fetch(`${videoDetailsURL}${data2.items[0].snippet.resourceId.videoId}%2C${data2.items[1].snippet.resourceId.videoId}%2C${data2.items[2].snippet.resourceId.videoId}%2C${data2.items[3].snippet.resourceId.videoId}%2C${data2.items[4].snippet.resourceId.videoId}%2C${data2.items[5].snippet.resourceId.videoId}%2C${data2.items[6].snippet.resourceId.videoId}&key=${apiKey}`)
+        this.setState({ novemberVideoList: data2 });
+        fetch(`${videoDetailsURL}${data2.items[0].snippet.resourceId.videoId}%2C${data2.items[1].snippet.resourceId.videoId}%2C${data2.items[2].snippet.resourceId.videoId}%2C${data2.items[3].snippet.resourceId.videoId}%2C${data2.items[4].snippet.resourceId.videoId}%2C${data2.items[5].snippet.resourceId.videoId}%2C${data2.items[6].snippet.resourceId.videoId}%2C${data2.items[7].snippet.resourceId.videoId}&key=${apiKey}`)
         .then((res3) => res3.json())
         .then(data3 => {
-          this.setState({ marchVideoDetails: data3})
-          this.setState({ marchVideosHere: true })          
+          this.setState({ novemberVideoDetails: data3})
+          this.setState({ novemberVideosHere: true })          
           fetch(`${playlistItemsURL}${data.items[1].id}&key=${apiKey}`)
           .then((res4) => res4.json())
           .then(data4 => {
-            this.setState({ aprilVideoList: data4 });
+            this.setState({ octoberVideoList: data4 });
             fetch(`${videoDetailsURL}${data4.items[0].snippet.resourceId.videoId}%2C${data4.items[1].snippet.resourceId.videoId}%2C${data4.items[2].snippet.resourceId.videoId}%2C${data4.items[3].snippet.resourceId.videoId}%2C${data4.items[4].snippet.resourceId.videoId}%2C${data4.items[5].snippet.resourceId.videoId}%2C${data4.items[6].snippet.resourceId.videoId}%2C${data4.items[7].snippet.resourceId.videoId}&key=${apiKey}`)
         .then((res5) => res5.json())
         .then(data5 => {
-          this.setState({ aprilVideoDetails: data5})
-          this.setState({ aprilVideosHere: true })
+          this.setState({ octoberVideoDetails: data5})
+          this.setState({ octoberVideosHere: true })
+          fetch(`${playlistItemsURL}${data.items[2].id}&key=${apiKey}`)
+          .then((res6) => res6.json())
+          .then(data6 => {
+            this.setState({ septemberVideoList: data6 });
+            fetch(`${videoDetailsURL}${data6.items[0].snippet.resourceId.videoId}%2C${data6.items[1].snippet.resourceId.videoId}%2C${data6.items[2].snippet.resourceId.videoId}%2C${data6.items[3].snippet.resourceId.videoId}%2C${data6.items[4].snippet.resourceId.videoId}%2C${data6.items[5].snippet.resourceId.videoId}%2C${data6.items[6].snippet.resourceId.videoId}%2C${data6.items[7].snippet.resourceId.videoId}&key=${apiKey}`)
+        .then((res7) => res7.json())
+        .then(data7 => {
+          this.setState({ septemberVideoDetails: data7})
+          this.setState({ septemberVideosHere: true })
             })
           })
         })
       })
     })
+  })
+})
       .catch(() => {  
         // eslint-disable-next-line react/no-unused-state
         this.setState({ isError: true });
@@ -98,7 +121,7 @@ class Goals extends React.Component {
   }
 
   render() {
-      if (this.state.marchVideosHere === false && this.state.aprilVideosHere === false) {
+      if (this.state.novemberVideosHere === false || this.state.octoberVideosHere === false || this.state.septemberVideosHere === false) {
         return (
         <div className= "loading-header">
           <div className="loading-wrap">
@@ -111,12 +134,16 @@ class Goals extends React.Component {
         )
       } else { 
         let goalsToDisplay = "";
-        if (this.state.displayMonth === "April") {
-          goalsToDisplay = this.state.aprilVideoDetails.items;
+        if (this.state.displayMonth === "September") {
+          goalsToDisplay = this.state.septemberVideoDetails.items;
+        }
+        else if (this.state.displayMonth === "October") {
+          goalsToDisplay = this.state.octoberVideoDetails.items;
         }
         else {
-          goalsToDisplay = this.state.marchVideoDetails.items;
+          goalsToDisplay = this.state.novemberVideoDetails.items;
         }
+        
       return (
         <Fragment>
            <MenuBar />
